3f6a61686581bf9e006cb0865ec1c100490b6956
UdaraDewshan <dewudara@gmail.com>
2025-12-18 22:46:19 +0530

    Add user interface to ToDoList FXML

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,6 +1,6 @@
-package edu.icet;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Starter extends Application {
@@ -10,6 +10,7 @@ public class Starter extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/TodoView.fxml"))));
+        stage.show();
     }
 }
